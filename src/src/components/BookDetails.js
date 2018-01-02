bedf03b6a3be97f0bbc7ab7adaaaa7639a2a140b
Ebenézer Silva <enebeze@hotmail.com>
2018-01-02 17:48:23 -0200

    Criação do componente BookDetails.js

---
+++ src/src/components/BookDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactLoading from "react-loading";
+import BookShelf from "./BookShelf";
+
+const BookDetails = () => { 
+
+    return (
+        <div>Teste</div>
+    );
+};
+
+export default BookDetails;
